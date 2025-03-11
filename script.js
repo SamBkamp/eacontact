4a926d1d2010090566a27b8fca68710f82e11f35
Sam Bonnekamp <s.bonnekamp@exotic-aqua.com>
2025-03-11 16:42:32 +0800

    added email submissiond data

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $("#emailForm").submit(function(e) {
     e.preventDefault();
+
+
+    var d = {
+	email: $("#exampleInputEmail1").val(),
+	name: $("#exampleInputPassword1").val(),
+	school: $("#schoolInput").val()
+    }
+    
+    console.log(d);
+    $.post("inventory.exotic-aqua.com/inventory", d, function(data){
+	
+    });
 });
+
